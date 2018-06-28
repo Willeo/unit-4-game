74244166db4e41ab2c12826d8a12e5c357f6566b
Willeo <willeo.smith@outlook.com>
2018-06-28 18:07:42 -0500

    need to add the random feature

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,116 @@
-var Yoda = {
-  name: "Yoda",
-  health: 110,
-  attack: 7,
-  counterAttack: 5
-};
+$(document).ready(function() {
+  var numberToGuess = 69;
 
-var Obiwan = {
-  name: "Obiwan",
-  health: 100,
-  attack: 10,
-  counterAttack: 7
-};
+  var counter = 0;
 
-var Bane = {
-  name: "Darth Bane",
-  health: 110,
-  attack: 10,
-  counterAttack: 7
-};
-var Dooku = {
-  name: "Darth Bane",
-  health: 90,
-  attack: 10,
-  counterAttack: 7
-};
-var player;
-var cpu;
+  var number1 = [10];
 
-// start game
-function game() {
-  var turn = true;
-}
-while (true) {
-  if (turn) {
-    game.player_1.playerMove();
-  } else {
-    game.player_2.playerMove();
+  var number2 = [5];
+
+  var number3 = [3];
+
+  var number4 = [7];
+
+  //var savedNum = $("button").text(Math.floor(Math.random() * 101));
+
+  $("number1 number2 number3 number4").text(numberToGuess);
+
+  for (var i = 0; i < number1.length; i++) {
+    var imageCrystal1 = $("<img>");
+
+    imageCrystal1.attr("data-num", number1[i]);
+
+    imageCrystal1.attr("src", "./assets/images/greenCrystal.jpg");
+
+    imageCrystal1.attr("alt", "green crystal");
+
+    imageCrystal1.addClass("crystalImage");
+
+    $("#crystals").append(imageCrystal1);
   }
-  turn = !turn;
-  // if player wins, select another enemy until no more enemy
-  // if player lose, start over
 
-  if (gameOver) {
-    break; // Break out of the loop if the game is over.
+  for (var i = 0; i < number2.length; i++) {
+    var imageCrystal2 = $("<img>");
+
+    imageCrystal2.attr("data-num", number2[i]);
+
+    imageCrystal2.attr("src", "./assets/images/blueCrystal.jpg");
+
+    imageCrystal2.attr("alt", "blue crystal");
+
+    imageCrystal2.addClass("crystalImage");
+
+    $("#crystals").append(imageCrystal2);
   }
-  turn = !turn;
 
-  // select character - increase size
-  // mouse click on the character for player
-  // mouse click on the opponet
+  for (var i = 0; i < number3.length; i++) {
+    var imageCrystal3 = $("<img>");
 
-  //
-  // if player health = 0, they lose
-  // if all cpu health = 0, computer win
+    imageCrystal3.attr("data-num", number3[i]);
 
-  // combat - player gets first attack - using attack stat.
+    imageCrystal3.attr("src", "./assets/images/redCrystal.jpg");
 
-  //Attack button - subtrack player attack# from enemy health
-  // subtrack enemy counter attack from player health
-  // update player and enemy health
+    imageCrystal3.attr("alt", "red crystal");
 
-  // battle continues until either player or enemy health === 0;
-}
-//combat
-var battle1 = function() {
-  return player.attackPower - enemy.health;
-};
-// button to start game.
-var battle2 = function() {};
+    imageCrystal3.addClass("crystalImage");
+
+    $("#crystals").append(imageCrystal3);
+  }
+
+  for (var i = 0; i < number4.length; i++) {
+    var imageCrystal4 = $("<img>");
+
+    imageCrystal4.attr("data-num", number4[i]);
+
+    imageCrystal4.attr("src", "./assets/images/purpleCrystal.jpg");
+
+    imageCrystal4.attr("alt", "purple crystal");
+
+    imageCrystal4.addClass("crystalImage");
+
+    $("#crystals").append(imageCrystal4);
+  }
+
+  $("#reset").on("click", function() {
+    reset();
+    Math.floor(Math.random() * 101);
+  });
+
+  $("button").click(function() {
+    $("h3").text(savedNum);
+  });
+
+  function reset() {
+    counter = 0;
+
+    $("#yourNumber").text(counter);
+
+    number1 = [5];
+
+    number2 = [3];
+
+    number3 = [7];
+
+    number4 = [10];
+
+    $("number1 number2 number3 number4").text(numberToGuess);
+  }
+
+  $(".crystalImage").on("click", function() {
+    counter = counter + parseInt($(this).data("num"));
+
+    $("#yourNumber").text(counter);
+
+    if (counter == numberToGuess) {
+      alert("You won!");
+
+      reset();
+
+      console.log("try it");
+    } else if (counter > numberToGuess) {
+      alert("You lost!");
+
+      reset();
+    }
+  });
+});
